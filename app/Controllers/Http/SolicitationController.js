3d1e1db2b3e7a318b00c58743a4de38b9bb251e6
isplatform <iscode2018@gmail.com>
2021-02-04 17:13:03 -0300

    teste

--- app/Controllers/Http/SolicitationController.js
+++ app/Controllers/Http/SolicitationController.js
@@ -18,7 +18,7 @@ class SolicitationController {
     async store({ request, response, view }) {
         //Comentario
 
-        // let intentName = request.body.queryResult.intent.displayName;
+      let intentName = request.body.queryResult.intent.displayName;
 
         if (intentName == "venda.salgados") {
             return response.json({
@@ -45,13 +45,13 @@ class SolicitationController {
         else if (intentName == "kit.grande") {
 
             const item = new SolicitationModel()
-            item.massas = request.queryResult.outputContexts.parameters['massas']
-            item.recheio = request.queryResult.outputContexts.parameters['recheio']
-            item.kitGrande = request.queryResult.outputContexts.parameters['kitGrande']
-            item.nomeCliente = request.queryResult.outputContexts.parameters['nomeCliente']
-            item.cep = request.queryResult.outputContexts.parameters['cep']
-            item.quantidade = request.queryResult.outputContexts.parameters['recheio']
-            item.data = request.queryResult.outputContexts.parameters['data']
+            item.massas = request.body.queryResult.outputContexts.parameters['massas']
+            item.recheio = request.body.queryResult.outputContexts.parameters['recheio']
+            item.kitGrande = request.body.queryResult.outputContexts.parameters['kitGrande']
+            item.nomeCliente = request.body.queryResult.outputContexts.parameters['nomeCliente']
+            item.cep = request.body.queryResult.outputContexts.parameters['cep']
+            item.quantidade = request.body.queryResult.outputContexts.parameters['recheio']
+            item.data = request.body.queryResult.outputContexts.parameters['data']
             await item.save()
         }
 
